5a6f9638b012492dbfce09b08341d121a1a523f7
cicbeast <raulgharringtonjr@gmail.com>
2020-06-09 22:48:29 -0400

    single number passes test

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,34 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
+# U - there will always be only one number that shows up once. All other numbers repeat once
 
-    pass
+# P - we can sort the values. This will place the repeats next to eachother.
+#     then we can check for repeats using an index loop 
+
+# E
+def single_number(arr):
+    arr = sorted(arr)
+    a = 0
+    while a < int(len(arr)/2):
+        if arr[(2*a)] != arr[(2*a +1)]:
+            return arr[2*a]
+        a +=1
+    # counter = 0
+    # checker = 0
+
+    # for i in arr:
+    #     a = 0
+    #     checker = i
+
+    #     while a < len(arr):
+    #         if arr[a] == checker:
+    #             counter += 1
+            
+    #         a += 1
+
+    #     if counter < 2:
+    #         return checker
 
 
 if __name__ == '__main__':
