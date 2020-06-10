022c1edd5e5cdb971449f3d3f8eb363bd2d16114
cicbeast <raulgharringtonjr@gmail.com>
2020-06-10 17:35:18 -0400

    moving zeroes passes tests

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,23 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
+# U - we're counting the number of non zero elements in a list and moving those elements to the left of the list.
 
-    pass
+# P - we can use a counter for the number of elements, and use a method we've previously adopted to move them
+
+# E
+def moving_zeroes(arr):
+    # we instantiate the counter
+    counter = 0
+    # and now we rearrange the list
+    for i in range(len(arr)):
+        if arr[i] != 0: # we don't want to move zeroes
+            counter += 1
+            arr = [arr[i]] + arr[:i] + arr[i+1:]
+    
+    return(arr)
+
+
 
 
 if __name__ == '__main__':
